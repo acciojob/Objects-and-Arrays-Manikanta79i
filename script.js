b91180589bedacb740a6ef355c02bf82cec4748b
Manikanta Itikela <113256332+Manikanta79i@users.noreply.github.com>
2023-04-10 13:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
-
-const person = {
-  name: "John Doe",
-  age: 80,
+let players = ["John", "Bob", "Alice", "Poppy"];
+let person = {
+name: "John Doe",
+age: 80,
 };
-
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
-
-let players = ["John", "Mike", "Sarah"];
-let person = {name: "Tom", age: 25};
 
 let team = players;
 let team1 = [...players];
 let cap1 = {...person};
-
 
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+
+// Testing
+console.log(players === team); // true
+console.log(players === team1); // false
+console.log(team1); // ["John", "Bob", "Alice", "Poppy"]
+console.log(cap1); // { name: "John Doe", age: 80 }
